2964ee536914b66d1bb0fe1a9cf000da0f60478a
Victor Lavrentyev <v.lavrenty@gmail.com>
2017-05-20 22:28:01 +0300

    master2

--- app/src/main/java/by/lvl/hexmap/HexmapApp.java
+++ app/src/main/java/by/lvl/hexmap/HexmapApp.java
@@ -5,7 +5,7 @@ import android.app.Application;
 import by.lvl.hexmap.api.Api;
 import by.lvl.hexmap.api.repo.RepositoryFactory;
 
-// branch1
+// master2
 public class HexmapApp extends Application {
 
     @Override
